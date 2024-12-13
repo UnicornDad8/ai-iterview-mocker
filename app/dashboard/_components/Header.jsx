e8e77132ed675e04291bf9b03318330ab287cb6f
UnicornDad8 <unicorndad8@gmail.com>
2024-12-13 01:53:42 -0300

    Add Header component

---
+++ app/dashboard/_components/Header.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect } from 'react';
+import { UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+
+const Header = () => {
+  const path = usePathname();
+
+  useEffect(() => {
+    console.log(path);
+  }, []);
+ 
+  return (
+    <div className="flex items-center justify-between p-4 shadow-sm bg-secondary">
+      <Image src={"/logo.svg"} width={160} height={100} alt="logo" />
+      <ul className="hidden gap-6 cursor-pointer md:flex">
+        <li className={`transition-all hover:text-primary hover:font-bold ${path === "/dashboard" && "text-primary font-bold"}`}>Dashboard</li>
+        <li className={`transition-all hover:text-primary hover:font-bold ${path === "/questions" && "text-primary font-bold"}`}>Questions</li>
+        <li className={`transition-all hover:text-primary hover:font-bold ${path === "/upgrade" && "text-primary font-bold"}`}>Upgrade</li>
+        <li className={`transition-all hover:text-primary hover:font-bold ${path === "/how" && "text-primary font-bold"}`}>How it works?</li>
+      </ul>
+      <UserButton />
+    </div>
+  )
+}
+
+export default Header;
