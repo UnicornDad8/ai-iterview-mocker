335317166531bb5304a1086fb0c0e45f9d504370
UnicornDad8 <unicorndad8@gmail.com>
2024-12-26 16:23:58 -0300

    Add state to close modal

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -9,6 +9,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
 
 
 
@@ -20,13 +21,17 @@ const AddNewInterview = () => {
       <div className="p-10 transition-all border rounded-lg cursor-pointer bg-secondary hover:scale-105 hover:shadow-md" onClick={() => setOpenDialog(true)}>
         <h2 className="text-lg font-semibold text-center">+ Add New</h2>
       </div>
-      <Dialog open={openDialog}>
+      <Dialog open={openDialog} onOpenChange={() => setOpenDialog(false)}>
         <DialogContent>
           <DialogHeader>
             <DialogTitle>Are you absolutely sure?</DialogTitle>
             <DialogDescription>
               This action cannot be undone. This will permanently delete your account
               and remove your data from our servers.
+              <span className="flex justify-end gap-5 mt-5">
+                <Button variant="ghost" onClick={() => setOpenDialog(false)}>Cancel</Button>
+                <Button>Start Interview</Button>
+              </span>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
