872f93935dfc783b107936a0fbc10f09808fac85
UnicornDad8 <unicorndad8@gmail.com>
2024-12-26 17:07:16 -0300

    Remove outline from Dialog component when focus

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -21,18 +21,18 @@ const AddNewInterview = () => {
       <div className="p-10 transition-all border rounded-lg cursor-pointer bg-secondary hover:scale-105 hover:shadow-md" onClick={() => setOpenDialog(true)}>
         <h2 className="text-lg font-semibold text-center">+ Add New</h2>
       </div>
-      <Dialog open={openDialog} onOpenChange={() => setOpenDialog(false)}>
+      <Dialog className="focus:outline-none ring-0 focus:ring-0" open={openDialog} onOpenChange={() => setOpenDialog(false)}>
         <DialogContent>
           <DialogHeader>
             <DialogTitle>Are you absolutely sure?</DialogTitle>
             <DialogDescription>
               This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-              <span className="flex justify-end gap-5 mt-5">
-                <Button variant="ghost" onClick={() => setOpenDialog(false)}>Cancel</Button>
-                <Button>Start Interview</Button>
-              </span>
+              and remove your data from our servers. 
             </DialogDescription>
+            <div className="flex justify-end gap-5 mt-10">
+                <Button className="outline-none" variant="ghost" onClick={() => setOpenDialog(false)}>Cancel</Button>
+                <Button>Start Interview</Button>
+            </div>
           </DialogHeader>
         </DialogContent>
       </Dialog>
