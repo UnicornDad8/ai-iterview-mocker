60302aa823c68b6563f0c626c36c97237ae1f70f
UnicornDad8 <unicorndad8@gmail.com>
2024-12-27 07:37:17 -0300

    Add onChange state to inputs on AddNewInterview component

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -17,6 +17,14 @@ import { Textarea } from "@/components/ui/textarea";
 
 const AddNewInterview = () => {
   const [openDialog, setOpenDialog] = useState(false);
+  const [jobPosition, setJobPosition] = useState();
+  const [jobDescription, setJobDescription] = useState();
+  const [jobExperience, setJobExperience] = useState();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(jobPosition, jobDescription, jobExperience);
+  };
 
   return (
     <div>
@@ -27,20 +35,34 @@ const AddNewInterview = () => {
         <DialogContent className="max-w-2xl">
           <DialogHeader>
             <DialogTitle className="text-2xl font-bold">Tell us more about your job interview</DialogTitle>
-            <form>
+            <form onSubmit={onSubmit}>
             <div>
               <h2>Add details about your job position or role, job description and years of experience</h2>
               <div className="my-3 mt-7">
                 <label>Job Role/Job Position</label>
-                <Input placeholder="Ex. Full-Stack Developer" required />
+                <Input 
+                  placeholder="Ex. Full-Stack Developer" 
+                  required 
+                  onChange={(event) => setJobPosition(event.target.value)}
+                />
               </div>
               <div className="my-3">
                 <label>Job Description/Tech Stack</label>
-                <Textarea placeholder="Ex. React, Angular, NodeJs, MySql, etc" required />
+                <Textarea 
+                  placeholder="Ex. React, Angular, NodeJs, MySql, etc" 
+                  required 
+                  onChange={(event) => setJobDescription(event.target.value)}
+                />
               </div>
               <div className="my-3">
                 <label>Years of experience</label>
-                <Input placeholder="Ex. 5" type="number" max="50" required />
+                <Input 
+                  placeholder="Ex. 5" 
+                  type="number" 
+                  max="50" 
+                  required
+                  onChange={(event) => setJobExperience(event.target.value)} 
+                />
               </div>
             </div>
             <div className="flex justify-end gap-5 mt-10">
