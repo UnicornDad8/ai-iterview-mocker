bdc2866c590f06a9dab2b58786366a7ea979a4c4
UnicornDad8 <unicorndad8@gmail.com>
2024-12-30 18:58:22 -0300

    Finish Interview component UI

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -5,7 +5,7 @@ import { db } from '@/utils/db';
 import { MockInterview } from '@/utils/schema';
 import { eq } from 'drizzle-orm';
 import Webcam from "react-webcam";
-import { WebcamIcon } from 'lucide-react';
+import { Lightbulb, WebcamIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
 const Interview = ({params}) => {
@@ -26,14 +26,18 @@ const Interview = ({params}) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center my-10">
+    <div className="flex flex-col justify-center my-10">
       <h2 className="mb-10 text-2xl font-bold">Let's get started</h2>
       <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
       <div className="flex flex-col gap-5">
         <div className="flex flex-col gap-5 p-5 border rounded-lg">
-        <h2 className="text-lg"><strong>Job Role/Job Position:</strong> {interviewData?.jobPosition}</h2>
-        <h2 className="text-lg"><strong>Job Description/Tech Stack:</strong> {interviewData?.jobDesc}</h2>
-        <h2 className="text-lg"><strong>Years of Experience:</strong> {interviewData?.jobExperience}</h2>
+          <h2 className="text-lg"><strong>Job Role/Job Position:</strong> {interviewData?.jobPosition}</h2>
+          <h2 className="text-lg"><strong>Job Description/Tech Stack:</strong> {interviewData?.jobDesc}</h2>
+          <h2 className="text-lg"><strong>Years of Experience:</strong> {interviewData?.jobExperience}</h2>
+        </div>
+        <div className="p-5 bg-yellow-100 border border-yellow-300 rounded-lg">
+          <h2 className="flex items-center gap-2 text-yellow-500"><Lightbulb /> <strong>Information</strong></h2>
+          <h2 className="mt-3 text-yellow-700">{process.env.NEXT_PUBLIC_INFORMATION}</h2>
         </div>
       </div>
 
@@ -50,12 +54,14 @@ const Interview = ({params}) => {
           /> :
           <div className="flex flex-col items-center">
             <WebcamIcon className="w-full p-20 border rounded-lg h-72 bg-secondary" />
-            <Button className="my-3" onClick={() => setWebcamEnabled(true)}>Enable webcam and microphone</Button>
+            <Button variant="ghost" className="w-full my-3" onClick={() => setWebcamEnabled(true)}>Enable webcam and microphone</Button>
           </div>
           }
       </div>
       </div>
-      
+      <div className="flex items-end justify-end mt-5">
+        <Button>Start Interview</Button>
+      </div>
     </div>
   )
 }
