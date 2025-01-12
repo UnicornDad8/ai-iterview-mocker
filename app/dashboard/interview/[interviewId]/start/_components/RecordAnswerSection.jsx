3cad3dd416acabaa03e66a2af9c753e596e64e6c
UnicornDad8 <unicorndad8@gmail.com>
2025-01-12 15:27:29 -0300

    Add ability to record answer from speech to text

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -1,13 +1,37 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import useSpeechToText from 'react-hook-speech-to-text';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
-import React from 'react'
 import Webcam from 'react-webcam';
+import { Mic } from 'lucide-react';
 
 const RecordAnswerSection = () => {
+  const [userAnswer, setUserAnswer] = useState("");
+
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false
+  });
+
+  useEffect(() => {
+    results.map((result) => (
+      setUserAnswer(prevAns => prevAns + result?.transcript)
+    ));
+  }, [results]);
+
   return (
     <div className="flex flex-col items-center justify-center">
         <div className="flex flex-col items-center justify-center p-5 mt-10 rounded-lg bg-secondary">
-      <Image src={"/webcam.png"} width={200} height={200} className="absolute" />
+      <Image src={"/webcam.png"} alt="webcam" width={200} height={200} className="absolute" />
       <Webcam 
         mirrored={true}
         style={{
@@ -17,7 +41,16 @@ const RecordAnswerSection = () => {
         }}
       />
     </div>
-    <Button variant="outline" className="my-10">Record Answer</Button>
+    <Button 
+      variant="outline" 
+      className="my-10"
+      onClick={isRecording ? stopSpeechToText : startSpeechToText}
+      >{isRecording ? 
+        <h2 className="flex items-center gap-2 text-red-600">
+          <Mic /> Stop Recording
+        </h2> : "Record Answer"
+      }</Button>
+    <Button onClick={() => console.log(userAnswer)}>Show User Answer</Button> 
     </div>
   )
 }
