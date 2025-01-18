3e88c7be83615c256de608f6c6b6b694c7077df8
UnicornDad8 <unicorndad8@gmail.com>
2025-01-18 13:14:59 -0300

    Add Feeback component header

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Feedback = () => {
   return (
-    <div>Feedback...</div>
+    <div className="p-10">
+      <h2 className="text-3xl font-bold text-green-500">Congratulation!</h2>
+      <h2 className="text-xl font-semibold">Here is your interview feedback</h2>
+      <h2 className="my-3 text-lg text-primary">Your overall interview rating: <strong>7/10</strong></h2>
+    
+      <h2 className="text-sm text-gray-500">Find below interview question with correct answer, your answer and feedback for improvement</h2>
+    </div>
   )
 }
 
