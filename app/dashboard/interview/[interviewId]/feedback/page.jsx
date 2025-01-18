22c640126249f809d9862da80bde7387b4f58815
UnicornDad8 <unicorndad8@gmail.com>
2025-01-18 13:55:51 -0300

    Fetch feedback on Feedback component

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+"use client";
 
-const Feedback = () => {
+import { db } from '@/utils/db'
+import { UserAnswer } from '@/utils/schema'
+import { eq } from 'drizzle-orm'
+import { useEffect, useState } from 'react'
+
+const Feedback = ({ params }) => {
+  const [feedbackList, setFeedbackList] = useState([]);
+  
+  useEffect(() => {
+    GetFeedback();
+  }, []);
+
+  const GetFeedback = async () => {
+    const result = await db.select().from(UserAnswer)
+      .where(eq(UserAnswer?.mockIdRef, params.interviewId))
+      .orderBy(UserAnswer.id);
+
+      console.log(result);
+      setFeedbackList(result);
+  }
+
   return (
     <div className="p-10">
       <h2 className="text-3xl font-bold text-green-500">Congratulation!</h2>
